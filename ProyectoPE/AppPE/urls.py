d635f5e6d147039e681b6987657c6fa650f8ee51
Paula Florio <pau.florio@gmail.com>
2022-06-04 13:30:08 -0300

    Fijando Buscadores

--- ProyectoPE/AppPE/urls.py
+++ ProyectoPE/AppPE/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 # from AppPE import views
-from AppPE.views import inicio, series, peliculas, libros, animes, mangas, serieFormulario, peliculaFormulario, libroFormulario, animeFormulario, mangaFormulario
+from AppPE.views import inicio, series, peliculas, libros, animes, mangas, serieFormulario, peliculaFormulario, libroFormulario, animeFormulario, mangaFormulario, busquedaPeliculaDirector, buscarPeliculaDirector, busquedaPeliculaAño, buscarPeliculaAño, busquedaSerieDirector, buscarSerieDirector,busquedaSerieAño, buscarSerieAño, busquedaLibroAutor, buscarLibroAutor, busquedaLibroAño, buscarLibroAño, busquedaMangaAutor, buscarMangaAutor, busquedaMangaAño, buscarMangaAño, busquedaAnimeDirector, busquedaAnimeAño, buscarAnimeDirector, buscarAnimeAño
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,5 +16,25 @@ urlpatterns = [
     path('libroFormulario/', libroFormulario, name='libroFormulario'),
     path('animeFormulario/', animeFormulario, name='animeFormulario'),
     path('mangaFormulario/', mangaFormulario, name='mangaFormulario'),
+    path('busquedaPeliculaDirector/', busquedaPeliculaDirector, name = 'busquedaPeliculaDirector'),
+    path('buscarPeliculaDirector/', buscarPeliculaDirector, name = 'buscarPeliculaDirector'),
+    path('busquedaPeliculaAño/', busquedaPeliculaAño, name = 'busquedaPeliculaAño'),
+    path('buscarPeliculaAño/', buscarPeliculaAño, name = 'buscarPeliculaAño'),
+    path('busquedaSerieDirector/', busquedaSerieDirector, name = 'busquedaSerieDirector'),
+    path('buscarSerieDirector/', buscarSerieDirector, name = 'buscarSerieDirector'),
+    path('busquedaSerieAño/', busquedaSerieAño, name = 'busquedaSerieAño'),
+    path('buscarSerieAño/', buscarSerieAño, name = 'buscarSerieAño'),
+    path('busquedaLibroAutor/', busquedaLibroAutor, name = 'busquedaLibroAutor'),
+    path('buscarLibroAutor/', buscarLibroAutor, name = 'buscarLibroAutor'),
+    path('busquedaLibroAño/', busquedaLibroAño, name = 'busquedaLibroAño'),
+    path('buscarLibroAño/', buscarLibroAño, name = 'buscarLibroAño'),
+    path('busquedaMangaAutor/', busquedaMangaAutor, name = 'busquedaMangaAutor'),
+    path('buscarMangaAutor/', buscarMangaAutor, name = 'buscarMangaAutor'),
+    path('busquedaMangaAño/', busquedaMangaAño, name = 'busquedaMangaAño'),
+    path('buscarMangaAño/', buscarMangaAño, name = 'buscarMangaAño'),
+    path('busquedaAnimeDirector/', busquedaAnimeDirector, name = 'busquedaAnimeDirector'),
+    path('buscarAnimeDirector/', buscarAnimeDirector, name = 'buscarAnimeDirector'),
+    path('busquedaAnimeAño/', busquedaAnimeAño, name = 'busquedaAnimeAño'),
+    path('buscarAnimeAño/', buscarAnimeAño, name = 'buscarAnimeAño'),
 
 ]
--- ProyectoPE/AppPE/views.py
+++ ProyectoPE/AppPE/views.py
@@ -105,3 +105,129 @@ def mangaFormulario(request):
     else:
         miFormulario = MangaFormulario()
     return render(request, 'appPE/mangaFormulario.html', {'miFormulario': miFormulario})
+
+def busquedaPeliculaDirector(request):
+    return render(request, 'appPE/busquedaPeliculaDirector.html')
+
+def buscarPeliculaDirector(request):
+    # respuesta = f"Se encontro el director {request.GET['director']}"
+    if request.GET['director']:
+        director = request.GET['director']
+        peliculas = Pelicula.objects.filter(director = director)
+        return render(request, 'appPE/resultadosBusquedaPeliculaDirector.html', {'peliculas': peliculas, 'director': director})
+    else:
+        respuesta = "No se han ingresado datos"
+    return HttpResponse(respuesta)
+
+def busquedaPeliculaAño(request):
+    return render(request, 'appPE/busquedaPeliculaAño.html')
+
+def buscarPeliculaAño(request):
+    if request.GET['añoEstreno']:
+        añoEstreno = request.GET['añoEstreno']
+        peliculas = Pelicula.objects.filter(añoEstreno = añoEstreno)
+        return render(request, 'appPE/resultadosBusquedaPeliculaAño.html', {'peliculas': peliculas, 'añoEstreno': añoEstreno})
+    else:
+        respuesta = "No se han ingresado datos"
+    return HttpResponse(respuesta)
+
+
+def busquedaSerieDirector(request):
+    return render(request, 'appPE/busquedaSerieDirector.html')
+
+def buscarSerieDirector(request):
+    if request.GET['director']:
+        director = request.GET['director']
+        series = Serie.objects.filter(director = director)
+        return render(request, 'appPE/resultadosSerieDirector.html', {'series': series, 'director': director})
+    else:
+        respuesta = "No se han ingresado datos"
+    return HttpResponse(respuesta)
+
+def busquedaSerieAño(request):
+    return render(request, 'appPE/busquedaSerieAño.html')
+
+def buscarSerieAño(request):
+    if request.GET['añoEstreno']:
+        añoEstreno = request.GET['añoEstreno']
+        series = Serie.objects.filter(añoEstreno = añoEstreno)
+        return render(request, 'appPE/resultadosSerieAño.html', {'series': series, 'añoEstreno': añoEstreno})
+    else:
+        respuesta = "No se han ingresado datos"
+    return HttpResponse(respuesta)
+
+
+def busquedaLibroAutor(request):
+    return render(request, 'appPE/busquedaLibroAutor.html')
+
+def buscarLibroAutor(request):
+    if request.GET['autor']:
+        autor = request.GET['autor']
+        libros = Libro.objects.filter(autor = autor)
+        return render(request, 'appPE/resultadosLibroAutor.html', {'libros': libros, 'autor': autor})
+    else:
+        respuesta = "No se han ingresado datos"
+    return HttpResponse(respuesta)
+
+def busquedaLibroAño(request):
+    return render(request, 'appPE/busquedaLibroAño.html')
+
+def buscarLibroAño(request):
+    if request.GET['añoPublicacion']:
+        añoPublicacion = request.GET['añoPublicacion']
+        libros = Libro.objects.filter(añoPublicacion = añoPublicacion)
+        return render(request, 'appPE/resultadosLibroAño.html', {'libros': libros, 'añoPublicacion': añoPublicacion})
+    else:
+        respuesta = "No se han ingresado datos"
+    return HttpResponse(respuesta)
+
+
+def busquedaMangaAutor(request):
+    return render(request, 'appPE/busquedaMangaAutor.html')
+
+def buscarMangaAutor(request):
+    if request.GET['autor']:
+        autor = request.GET['autor']
+        mangas = Manga.objects.filter(autor = autor)
+        return render(request, 'appPE/resultadosMangaAutor.html', {'mangas': mangas, 'autor': autor})
+    else:
+        respuesta = "No se han ingresado datos"
+    return HttpResponse(respuesta)
+
+def busquedaMangaAño(request):
+    return render(request, 'appPE/busquedaMangaAño.html')
+
+def buscarMangaAño(request):
+    if request.GET['añoPublicacion']:
+        añoPublicacion = request.GET['añoPublicacion']
+        mangas = Manga.objects.filter(añoPublicacion = añoPublicacion)
+        return render(request, 'appPE/resultadosMangaAño.html', {'mangas': mangas, 'añoPublicacion': añoPublicacion})
+    else:
+        respuesta = "No se han ingresado datos"
+    return HttpResponse(respuesta)
+
+
+
+def busquedaAnimeDirector(request):
+    return render(request, 'appPE/busquedaAnimeDirector.html')
+
+def buscarAnimeDirector(request):
+    if request.GET['director']:
+        director = request.GET['director']
+        animes = Anime.objects.filter(director = director)
+        return render(request, 'appPE/resultadosAnimeDirector.html', {'animes': animes, 'director': director})
+    else:
+        respuesta = "No se han ingresado datos"
+    return HttpResponse(respuesta)
+
+def busquedaAnimeAño(request):
+    return render(request, 'appPE/busquedaAnimeAño.html')
+
+def buscarAnimeAño(request):
+    if request.GET['añoEstreno']:
+        añoEstreno = request.GET['añoEstreno']
+        animes = Anime.objects.filter(añoEstreno = añoEstreno)
+        return render(request, 'appPE/resultadosAnimeAño.html', {'animes': animes, 'añoEstreno': añoEstreno})
+    else:
+        respuesta = "No se han ingresado datos"
+    return HttpResponse(respuesta)
