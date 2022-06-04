6f0065895de03b18f3c0568e89efdc62d594dae7
Paula Florio <pau.florio@gmail.com>
2022-06-03 23:46:34 -0300

    Herencia de Templates

--- ProyectoPE/AppPE/urls.py
+++ ProyectoPE/AppPE/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 # from AppPE import views
-from AppPE.views import inicio
+from AppPE.views import inicio, series, peliculas, libros, animes, mangas
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', inicio),
+    path('', inicio, name='Inicio'),
+    path('series/', series, name = 'Series'),
+    path('peliculas/', peliculas, name = 'Peliculas'),
+    path('libros/', libros, name = 'Libros'),
+    path('animes/', animes, name = 'Animes'),
+    path('mangas/', mangas, name = 'Mangas'),
+
 ]
--- ProyectoPE/AppPE/views.py
+++ ProyectoPE/AppPE/views.py
@@ -6,3 +6,18 @@ def inicio(self):
     plantilla = loader.get_template('AppPE/inicio.html')
     documento = plantilla.render()
     return HttpResponse(documento)
+
+def series(request):
+    return render(request, 'appPE/series.html')
+
+def peliculas(request):
+    return render(request, 'appPE/peliculas.html')
+
+def libros(request):
+    return render(request, 'appPE/libros.html')
+
+def animes(request):
+    return render(request, 'appPE/animes.html')
+
+def mangas(request):
+    return render(request, 'appPE/mangas.html')
